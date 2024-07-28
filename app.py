ce65ca7d7e0fbbd964fa66edba50c45cb2393378
beatriz-lafuente <beatriz.fuente.santos@gmail.com>
2024-07-28 23:01:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,18 @@ with col1:
     kpi = st.selectbox("Select KPI", list(kpi_options.keys()))
     kpi_code = kpi_options[kpi]
     
+
+def params_to_query_string(params):
+    return '&'.join([f'{key}={",".join(value)}' for key, value in params.items()])
+
 with col2:
-    api_url = f"http://localhost:8000/charts/dash1_inclusion_q11?kpi={kpi_code}"
+    nat_params = {
+    'geo': ['FR', 'FI', 'PT', 'DE', 'SK']
+    }
+    query_params = params_to_query_string(nat_params)
+    print(query_params)
+    api_url = f"http://localhost:8000/charts/dash1_inclusion_q11/?kpi={kpi_code}&{query_params}"
+    
     response = requests.get(api_url)
 
     if response.status_code == 200:
