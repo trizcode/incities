b9a814efa36dd4e38afcefd009232385e48c5e09
beatriz-lafuente <beatriz.fuente.santos@gmail.com>
2024-09-19 23:16:42 +0100

    Remove files

--- utils.py
+++ utils.py
@@ -1,43 +1,76 @@
-import pandas as pd
 import requests
 import streamlit as st
+import plotly.io as pio
 from streamlit_echarts import st_echarts
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 
-# Sub dimensions and topics
-sustainability_topics = ["Air Quality", "Energy", "Biodiversity", "Waste Management", "Employment"]
-resilience_dim = ["Social", "Economic & Infrastructure"]
+# Define domains
+domain_list = ["Inclusion", "Sustainability", "Resilience"]
 
+# Define sub-domains
+sub_domain_inclusion = ["Social", "Economic", "Gender"]
 
-# KPIs
-# --> Inclusion
-inclusion_kpis = {
-  "Gini coefficient": "tessi190",
-  "Disability employment gap": "tepsr_sp200",
-  "Risk of poverty rate": "tespm010",
-  "Gender employment gap": "tepsr_lm220"
+sub_domain_sustainability = ["Environmental", "Social", "Economic"]
+
+sub_domain_resilience = ["Social", "Economic", "Infrastructure", "Institutional", "Hazard"]
+
+# Define sub-domain kpis
+social_inclusion_kpis = { 
+    "Disability employment gap": "tepsr_sp200",
+    "Youth Unemployment": "edat_lfse_22",
+    "Slum Household": "ilc_lvhl21n"
 }
-# --> Sustainability
+
+economic_inclusion_kpis = {
+    "Gini coefficient": "tessi190",
+    "Poverty Rate": "ilc_li41"
+}
+
+gender_inclusion_kpis = {
+    "Gender employment gap": "tepsr_lm220"
+}
+
+environment_sustainability_topics = ["Air Quality", "Energy", "Biodiversity", "Environmental quality"]
+
+economic_sustainability_topics = ["Employment", "Infrastructure", "Innovation"]
+
+social_sustainability_topics = ["Safety", "Education"]
+
+social_resilience_topics = ["Educational equality", "Demography"]
+
+economic_resilience_topics = ["Health access", "Market access"]
+
 air_quality_kpis = {
-  "Greenhouse gases emissions": "cei_gsr011",
-  "Average CO2 emissions": "sdg_12_30"
+    "Greenhouse gas (GHG) emissions": "cei_gsr011",
+    "Concentration of NO2 (µg/m³)": "no2", 
+    "Concentration of PM10 (µg/m³)": "pm10",
 }
+
 energy_kpis = {
+    "Renewable energy sources (Total)": "REN",
     "Renewable energy sources in transport": "REN_TRA",
     "Renewable energy sources in electricity": "REN_ELC",
     "Renewable energy sources in heating and cooling": "REN_HEAT_CL",
 }
-# --> Resilience
-resilience_kpis = {
-    "Number of Physicians": "hlth_rs_physreg",
-    "Available beds in hospitals": "tgs00064",
-    "Regional gross domestic product": "tgs00006"
+
+economic_sustainability_kpis = {
+    "Persons employed in productive age": "tgs00007",
+    "Household level of internet access": "tgs00047",
+    "R&D personnel and researchers": "rd_p_persreg"
 }
 
-# Geo levels
-# --> Countries dictionary
+health_kpis = {
+    "Share of Total deaths": "hlth_cd_yro",
+    "Infant mortality": "hlth_cd_yinfr"
+}
+
+demography_kpis = {
+    "Population in productive age": "demo_r_pjangrp3",
+    "Population aged 65 years and older": "demo_r_pjangrp3_aged",
+    "Population density": "demo_r_d3dens",
+}
+
+# Define regions
 nat_dict = {
     "Finland": "FI",
     "Portugal": "PT",
@@ -45,7 +78,7 @@ nat_dict = {
     "France": "FR", 
     "Germany": "DE"
 }
-# --> Nuts 2 regions dictionary
+
 nuts2_dict = {
     "Köln": "DEA2",
     "Helsinki-U.": "FI1B",
@@ -53,54 +86,48 @@ nuts2_dict = {
     "A. M. Lisboa": "PT17",
     "Ile de France": "FR10"
 }
-# --> Nuts 3 regions dictionary
-nuts3_dict = {
-    "Helsinki": "FI001C", 
-    "Lisbon": "PT001C", 
-    "Paris": "FR001C",
-    "Köln": "DE004C",
-    "Zilina": "SK006C"
-}
 
 
-# Functions to display echarts visualizations
-# --> General chart functions
-def echarts_option(echarts_function, kpi):
+# Functions to display data visualizations
+def echarts_option(echarts_function, key_name, kpi):
     
-    response = requests.get(f'http://localhost:8000/data_charts/{echarts_function}/?dataset_code={kpi}')
+    response = requests.get(f'http://localhost:8000/data_charts/{echarts_function}/?{key_name}={kpi}')
     chart_option = response.json()
-    st_echarts(options=chart_option, height="400px")
+    st_echarts(options=chart_option, height="500px")
+
+
+def echarts_option_without_kpi(echarts_function):
+    
+    response = requests.get(f'http://localhost:8000/data_charts/{echarts_function}/')
+    chart_option = response.json()
+    st_echarts(options=chart_option, height="500px")
+    
 
 def echarts_option_kpi(echarts_function, dataset_code, col_kpi, kpi):
     response = requests.get(f"http://localhost:8000/data_charts/{echarts_function}/?dataset_code={dataset_code}&{col_kpi}={kpi}")
     chart_option = response.json()
-    st_echarts(options=chart_option, height="400px")
+    st_echarts(options=chart_option, height="500px")
 
-def echarts_option_w_kpi(echarts_function):
-    response = requests.get(f"http://localhost:8000/data_charts/{echarts_function}/")
-    chart_option = response.json()
-    st_echarts(options=chart_option, height="400px")
 
-# --> Specific chart functions
-def scatter_plot_gini_vs_poverty():
-    response = requests.get(f'http://localhost:8000/data_charts/scatter_plot_gini_vs_poverty/')
-    df = pd.DataFrame(response.json())
-    sns.set_theme(style="white")
-    plt.figure(figsize=(10, 6))
+def plotly_chart(plotly_chart, key_name, kpi):
     
-    sns.scatterplot(
-        x="values_gini",
-        y="values_poverty",
-        hue="geo",
-        sizes=(40, 400),
-        alpha=0.5,
-        palette="muted",
-        data=df
-    )
-    
-    plt.xlabel('Gini Coefficient (%)')
-    plt.ylabel('Risk of Poverty Rate (%)')
-    plt.title('Scatter Plot of Gini Coefficient vs. Risk of Poverty Rate')
-    
-    st.pyplot(plt)
+    response = requests.get(f'http://localhost:8000/data_charts/{plotly_chart}/?{key_name}={kpi}')
+    fig_json = response.json()
+    fig = pio.from_json(fig_json)
+    st.plotly_chart(fig)
 
+
+def plotly_chart_without_kpi(plotly_chart):
+    
+    response = requests.get(f'http://localhost:8000/data_charts/{plotly_chart}/')
+    fig_json = response.json()
+    fig = pio.from_json(fig_json)
+    st.plotly_chart(fig)
+    
+    
+def plotly_chart_cards(plotly_chart, key_name, kpi):
+
+    response = requests.get(f'http://localhost:8000/data_charts/{plotly_chart}/?{key_name}={kpi}')
+    figures_json = response.json()
+    figures = [pio.from_json(fig_json) for fig_json in figures_json]
+    return figures
